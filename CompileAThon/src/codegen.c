364eac669ac92eede05c7e94bd36d4d6959f3750
Archis2004 <archisman0404@gmail.com>
2025-03-29 12:32:20 +0530

    Parallelized the flow.

--- CompileAThon/src/codegen.c
+++ CompileAThon/src/codegen.c
@@ -21,6 +21,11 @@ void generate_instruction_sequence(const MatrixIR *ir, const OptimizedInfo *opt)
         #pragma omp for collapse(2) nowait
         for (int i = 0; i < ir->rows_A; i++) {
             for (int j = 0; j < ir->cols_B; j++) {
+                #pragma omp critical
+                {
+                    fprintf(file, "Processing C[%d][%d]\n", i, j);
+                }
+
                 for (int k = 0; k < ir->cols_A; k++) {
                     Instruction inst_read_A = assemble_instruction(INST_NOP, 0, 1, 0, compute_memory_address("A", i, k));
                     Instruction inst_read_B = assemble_instruction(INST_NOP, 0, 1, 0, compute_memory_address("B", k, j));
@@ -29,15 +34,18 @@ void generate_instruction_sequence(const MatrixIR *ir, const OptimizedInfo *opt)
                     {
                         fprintf(file, "READ A[%d][%d]: 0x%06X\n", i, k, inst_read_A);
                         fprintf(file, "READ B[%d][%d]: 0x%06X\n", k, j, inst_read_B);
-
-                        Instruction inst_prog = assemble_instruction(INST_PROG, 0x0A, 0, 0, 0);
-                        Instruction inst_exe = assemble_instruction(INST_EXE, 0x14, 0, 0, 0);
-                        Instruction inst_end = assemble_instruction(INST_END, 0, 0, 0, 0);
-
-                        fprintf(file, "PROG: 0x%06X\n", inst_prog);
-                        fprintf(file, "EXE: 0x%06X\n", inst_exe);
-                        fprintf(file, "END: 0x%06X\n", inst_end);
                     }
+                }
+
+                #pragma omp critical
+                {
+                    Instruction inst_prog = assemble_instruction(INST_PROG, 0x0A, 0, 0, 0);
+                    Instruction inst_exe = assemble_instruction(INST_EXE, 0x14, 0, 0, 0);
+                    Instruction inst_end = assemble_instruction(INST_END, 0, 0, 0, 0);
+
+                    fprintf(file, "PROG: 0x%06X\n", inst_prog);
+                    fprintf(file, "EXE: 0x%06X\n", inst_exe);
+                    fprintf(file, "END: 0x%06X\n", inst_end);
                 }
             }
         }
